20b9feed2c14ef8d85f50627a70653860657759c
Asma Farheen <asmafarheen1510@gmail.com>
2021-06-26 23:59:54 +0530

    updates

--- public/javascripts/bmi.js
+++ public/javascripts/bmi.js
@@ -1,127 +1,219 @@
 
+// function data()
+// {
+//     var height=document.getElementsByClassName("height")[0].value;
+//     var weight=document.getElementsByClassName("height")[1].value;
+    
+//     console.log(height,weight)
+//     console.log(result[0].height)
+//     $.ajax({
+//         url: '/personal-details',
+//         method: 'POST',
+//         data: {
+//             height:height,
+//             weight:weight
+//         },
+        
+//         success: function ( result )
+//         {
+//             console.log(result)
+//            console.log(result[0].height)
+//            //localStorage.userdetails=result
+//            localStorage.setItem("height", result[0].height);
+//            localStorage.setItem("weight", result[1].weight);
+//            //localStorage.setItem("userid", result[0]._id);
+//            window.location.href="/main"
+//         },
+//         error: function ( error )
+//         {
+            
+//         }
+//     })
+// }
+// //     console.log(height);
+// //     console.log(weight);
+// //     height=height/100;
+// //     console.log(height);
+// //     var bm=weight/Math.pow(height,2);
+// //     console.log(bm);
+// //     var status;
+// //     if(document.getElementById("veg").checked==true)
+// //     {
+// //         status="veg";
+// //     }
+// //     else if(document.getElementById("nonveg").checked==true)
+// //     {
+// //         status="nonveg";
+// //     }
+// //     else if(document.getElementById("egg").checked==true)
+// //     {
+// //         status="egg";
+// //     }
+// //     console.log(status)
+// //     var food;
+// //     if(bm<18.5 )
+// //     {
+// //         food="Under-weight";
+
+// //     }
+// //     else if(bm>=18.5 && bm<=24.9)
+// //     {
+// //         food="Healthy";
+// //     }
+// //     else{
+// //         food="Over-weight";
+// //     }
+// //     console.log(food);
+    
+// //     window.location.href="/login";
+
+// // }
+// //   function diet(var food, var status){
+
+    
+// //     if(food=="Over-weight")
+// //     {
+// //         if(status=="veg")
+// //         {
+          
+// //         }
+// //         else if(status=="nonveg")
+// //         {
+
+// //         }
+// //         else 
+// //         {
+
+// //         }
+        
+// //     }
+// //     else  if(food=="Under-weight")
+// //     {
+// //         if(status=="veg")
+// //         {
+
+// //         }
+// //         else if(status=="nonveg")
+// //         {
+
+// //         }
+// //         else 
+// //         {
+
+// //         }
+        
+// //     }
+// //     else if(food=="Healthy")
+// //     {
+// //         if(status=="veg")
+// //         {
+// //             console.log("successveg") 
+            
+// //         }
+// //         else if(status=="nonveg")
+// //         {
+// //             console.log("success")  
+// //             window.location.href="/login";
+           
+// //               }
+        
+// //         else 
+// //         {
+            
+// //         }
+// //     }
+// //}
 function data()
 {
     var height=document.getElementsByClassName("height")[0].value;
     var weight=document.getElementsByClassName("height")[1].value;
-    
     console.log(height,weight)
-    console.log(result[0].height)
-    $.ajax({
-        url: '/personal-details',
-        method: 'POST',
-        data: {
-            height:height,
-            weight:weight
-        },
-        
-        success: function ( result )
-        {
-            console.log(result)
-           console.log(result[0].height)
-           //localStorage.userdetails=result
-           localStorage.setItem("height", result[0].height);
-           localStorage.setItem("weight", result[1].weight);
-           //localStorage.setItem("userid", result[0]._id);
-           window.location.href="/main"
-        },
-        error: function ( error )
-        {
-            
-        }
-    })
+    localStorage.setItem('myheight', height);
+    localStorage.setItem('myweight', weight);
+var hei = localStorage.getItem('myheight');
+var wei = localStorage.getItem('myweight');
+console.log(hei);
+console.log(wei);
+    hei=hei/100;
+    console.log(hei);
+    var bm=weight/Math.pow(hei,2);
+    localStorage.setItem('mybm',bm);
+    var b= localStorage.getItem('mybm');
+    console.log(b);
+    var status;
+    if(document.getElementById("veg").checked==true)
+    {
+        status="veg";
+    }
+    else if(document.getElementById("nonveg").checked==true)
+    {
+        status="nonveg";
+    }
+    else if(document.getElementById("egg").checked==true)
+    {
+        status="egg";
+    }
+    localStorage.setItem('mystatus',status);
+    var stat=localStorage.getItem('mystatus');
+    console.log(stat);
+    var food;
+    if(bm<18.5 )
+    {
+        food="Under-weight";
+    }
+    else if(bm>=18.5 && bm<=24.9)
+    {
+        food="Healthy";
+    }
+    else{
+        food="Over-weight";
+    }
+    localStorage.setItem('myfood',food);
+    var foodie=localStorage.getItem('myfood');
+    console.log(foodie);
+    window.location.href="/login";
 }
-//     console.log(height);
-//     console.log(weight);
-//     height=height/100;
-//     console.log(height);
-//     var bm=weight/Math.pow(height,2);
-//     console.log(bm);
-//     var status;
-//     if(document.getElementById("veg").checked==true)
-//     {
-//         status="veg";
-//     }
-//     else if(document.getElementById("nonveg").checked==true)
-//     {
-//         status="nonveg";
-//     }
-//     else if(document.getElementById("egg").checked==true)
-//     {
-//         status="egg";
-//     }
-//     console.log(status)
-//     var food;
-//     if(bm<18.5 )
-//     {
-//         food="Under-weight";
-
-//     }
-//     else if(bm>=18.5 && bm<=24.9)
-//     {
-//         food="Healthy";
-//     }
-//     else{
-//         food="Over-weight";
-//     }
-//     console.log(food);
+  function diet(){
+    var sta=localStorage.getItem('mystatus');
+    var foo=localStorage.getItem('myfood');
+    console.log(sta);
+    console.log(foo);
     
-//     window.location.href="/login";
-
-// }
-//   function diet(var food, var status){
-
-    
-//     if(food=="Over-weight")
-//     {
-//         if(status=="veg")
-//         {
+    if(foo=="Over-weight")
+    {
+        if(sta=="veg")
+        {
+            window.location.href="/ov";
+        }
+        else 
+        {
+            window.location.href="/on";
+        }
+    }
+    else  if(foo=="Under-weight")
+    {
+        if(sta=="veg")
+        {
+            window.location.href="/uv";
+        }
+        else 
+        {
+            window.location.href="/un";
+        }    
+    }
+    else if(foo=="Healthy")
+    {
+        if(sta=="veg")
+        {
+            window.location.href="/hv";
+        }
+        else 
+        {
           
-//         }
-//         else if(status=="nonveg")
-//         {
-
-//         }
-//         else 
-//         {
-
-//         }
-        
-//     }
-//     else  if(food=="Under-weight")
-//     {
-//         if(status=="veg")
-//         {
-
-//         }
-//         else if(status=="nonveg")
-//         {
-
-//         }
-//         else 
-//         {
-
-//         }
-        
-//     }
-//     else if(food=="Healthy")
-//     {
-//         if(status=="veg")
-//         {
-//             console.log("successveg") 
-            
-//         }
-//         else if(status=="nonveg")
-//         {
-//             console.log("success")  
-//             window.location.href="/login";
+          window.location.href="/hn";
            
-//               }
+       }
         
-//         else 
-//         {
-            
-//         }
-//     }
-//}
-
+    }
+}
 
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,56 @@
+// var express = require('express');
+// var router = express.Router();
+// var path=require('path')
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   let main=path.join(__dirname,"/../public/html/index.html");
+//   res.sendFile(main);
+  
+// });
+// router.get('/login', function(req, res, next) {
+//   let login=path.join(__dirname,"/../public/html/login.html");
+//   res.sendFile(login);
+  
+// });
+// router.get('/register', function(req, res, next) {
+//   let login=path.join(__dirname,"/../public/html/register.html");
+//   res.sendFile(login);
+  
+// });
+// router.get('/personaldetails', function(req, res, next) {
+//   let personaldetails=path.join(__dirname,"/../public/html/personal-details.html");
+//   res.sendFile(personaldetails);
+// });
+// router.get('/bodymass', function(req, res, next) {
+//   let bodymass=path.join(__dirname,"/../public/html/body-mass.html");
+//   res.sendFile(bodymass);
+// });
+// router.get('/main', function(req, res, next) {
+//   let k=path.join(__dirname,"/../public/html/main.html");
+//   res.sendFile(k);
+// });
+// router.get('/hn', function(req, res, next) {
+//   let hn=path.join(__dirname,"/../public/html/hn.html");
+//   res.sendFile(hn);
+// });
+// router.get('/hv', function(req, res, next) {
+//   let hv=path.join(__dirname,"/../public/html/hv.html");
+//   res.sendFile(hv);
+// });
+
+// router.get('/on', function(req, res, next) {
+//   let on=path.join(__dirname,"/../public/html/on.html");
+//   res.sendFile(on);
+// });
+
+// router.get('/ov', function(req, res, next) {
+//   let ov=path.join(__dirname,"/../public/html/ov.html");
+//   res.sendFile(ov);
+// });
+
+
+// module.exports = router;
 var express = require('express');
 var router = express.Router();
 var path=require('path')
@@ -48,6 +101,18 @@ router.get('/ov', function(req, res, next) {
   let ov=path.join(__dirname,"/../public/html/ov.html");
   res.sendFile(ov);
 });
+router.get('/un', function(req, res, next) {
+  let un=path.join(__dirname,"/../public/html/un.html");
+  res.sendFile(un);
+});
 
+router.get('/uv', function(req, res, next) {
+  let uv=path.join(__dirname,"/../public/html/uv.html");
+  res.sendFile(uv);
+});
+router.get('/blog', function(req, res, next) {
+  let blog=path.join(__dirname,"/../public/html/blog.html");
+  res.sendFile(blog);
+});
 
 module.exports = router;
